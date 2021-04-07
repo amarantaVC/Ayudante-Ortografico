535facf16af88da099ddae3c82e240c8bfaccf77
Amaranta <villegasamaranta@gmail.com>
2021-04-06 21:27:46 -0400

    señor, se logro la distancia levenstein

--- ayudante_ortografico.py
+++ ayudante_ortografico.py
@@ -22,41 +22,82 @@ from open_htable import OpenHtable
 import pmli
 from pmli import PMLI
 import string
+import numpy as np
+from numpy import*
 
-
+"Ayudante_Ortografico es un TAD que posee un arreglo con 27 PMLI que corresponde a cada letra del alfabeto latin y tiene como funcion almacenar las palabras del diccionario que se van a usar "
 class Ayudante_Ortografico(object):
     
-
+    assert(True)
+    #inciailizamos la estructura dicc con las 27 instancias de PMLI
     def __init__(self):
         self.alfabeto = list(string.ascii_lowercase)
         self.alfabeto.append('ñ')
         self.MAX = 27
         self.dicc = [PMLI(letra) for letra in self.alfabeto]
     
+    def crearAyudante(self) -> "Ayudante_Ortografico":
+        return self
+
+    "cargarDiccionario tiene la tarea de leer un archivo de entrada con las palabras de un diccionario y almacenarlas en la estructura dicc"
+    #{pre: fn}
     def cargarDiccionario(self, fname):
         archivo = open(fname,"r")
         with archivo as fp: 
             #readlines lee las lineas del archivo
             dicci = fp.readlines()
+
         for linea in dicci:
-            #strip quita los espacios
+
             linea = linea.strip()
-            #linea[0] significa primera letra de la palabra
-            #index entrega la posicion para obtener el PMLI del diccionario
-            index = self.alfabeto.index(linea[0])
-            buscar = self.dicc[index].buscarPalabra(linea)
-            if buscar == False:
 
-                self.dicc[index].agregarPalabra(linea)
+            if esPalabraValida(linea) :
+                    #strip quita los espacios
+                    #linea[0] significa primera letra de la palabra
+                    #index entrega la posicion para obtener el PMLI del diccionario
+                    index = self.alfabeto.index(linea[0])
+                    buscar = self.dicc[index].buscarPalabra(linea)
+                    if buscar == False:
+
+                        self.dicc[index].agregarPalabra(linea)
+                    else:
+                        pass
             else:
-                pass
-
+                print(f"archivo invalido por palabra no valida: {linea}")    
+    
+    "borrarPalabra tiene como tarea recibir una palabra y verificar si esta se encuentra en la estructura dicc, si esto es verdad procede a eliminar la palabra "
     def borrarPalabra(self,p):
+        #precondicion
         assert(esPalabraValida(p) == True)
 
         index = self.alfabeto.index(p[0])
         self.dicc[index].eliminarPalabra(p)
 
+    def levenshtein_distance(self,s1,s2):
+        size_s1 = len(s1) + 1
+        size_s2 = len(s2) +1
+
+        m = np.zeros((size_s1,size_s2))
+        
+        for i in range(size_s1):
+            m[i,0] = i
+        
+        for j in range(size_s2):
+            m[0,j] = j 
+
+        for i in range(1,size_s1):
+            for j in range(1,size_s2):
+                if s1[i-1] == s2[j-1]:
+                    diferencia = 0
+                else:
+                    diferencia = 1
+                imenos1 = m[i-1,j] + 1 #[i-1,j]
+                imenos1_jmenos1 = m[i-1,j-1] + diferencia #[i-1,j-1]
+                jmenos1 = m[i,j-1] + 1 #[i,j-1]
+                m[i,j] = min(imenos1,imenos1_jmenos1,jmenos1)
+        return (int(m[size_s1 - 1, size_s2 - 1]))
+    
+    "corregirTexto recibe como entrada un archivo co"
     def corregirTexto(self,finput):
         archivo = open(finput,"r")
         with archivo as fp:
@@ -74,19 +115,18 @@ class Ayudante_Ortografico(object):
                     pass
         
 
-
-
     def __str__(self):
         return f"{self.dicc}"
 
 
 a = Ayudante_Ortografico()
-a.cargarDiccionario('prueba.txt')
 
-#a.borrarPalabra("manzana")
+a.cargarDiccionario('prueba.txt')
+a.levenshtein_distance("cat","dog")
+#a.borrarPalabra("maria")
 
 #a.corregirTexto("corregir.txt")
 
-for diccionario in a.dicc:
-    if diccionario.palabras:
-        diccionario.mostrarPalabras()
+#for diccionario in a.dicc:
+    #if diccionario.palabras:
+        #diccionario.mostrarPalabras()
