abf57e486d4a6986393b612a042ea0027ae0f9e6
Amaranta <villegasamaranta@gmail.com>
2021-04-07 21:45:36 -0400

    ayudante con ImprimirDiccionario, se puede

--- ayudante_ortografico.py
+++ ayudante_ortografico.py
@@ -25,7 +25,8 @@ import string
 import numpy as np
 from numpy import*
 import collections
-
+import time 
+from time import perf_counter
 "Ayudante_Ortografico es un TAD que posee un arreglo con 27 PMLI que corresponde a cada letra del alfabeto latin y tiene como funcion almacenar las palabras del diccionario que se van a usar "
 class Ayudante_Ortografico(object):
     
@@ -36,22 +37,25 @@ class Ayudante_Ortografico(object):
         self.alfabeto.append('ñ')
         self.MAX = 27
         self.dicc = [PMLI(letra) for letra in self.alfabeto]
-    
+    "crearAyudante tiene la tarea de crear un nuevo TAD Ayudante Ortogradico en donde la estructura de datos dicc se inicializa "
     def crearAyudante(self) -> "Ayudante_Ortografico":
+        #precondicion
+        assert(True)
+
         return self
 
     "cargarDiccionario tiene la tarea de leer un archivo de entrada con las palabras de un diccionario y almacenarlas en la estructura dicc"
-    #{pre: fn}
-    def cargarDiccionario(self, fname):
+    
+    def cargarDiccionario(self, fname: str):
         archivo = open(fname,"r")
         with archivo as fp: 
             #readlines lee las lineas del archivo
             dicci = fp.readlines()
 
         for linea in dicci:
-
+            #preguntarRR 
             linea = linea.strip()
-
+            #precondicion
             if esPalabraValida(linea) :
                     #strip quita los espacios
                     #linea[0] significa primera letra de la palabra
@@ -59,7 +63,7 @@ class Ayudante_Ortografico(object):
                     index = self.alfabeto.index(linea[0])
                     buscar = self.dicc[index].buscarPalabra(linea)
                     if buscar == False:
-
+                        #postcondicion
                         self.dicc[index].agregarPalabra(linea)
                     else:
                         pass
@@ -84,7 +88,7 @@ class Ayudante_Ortografico(object):
             m[i,0] = i
         
         for j in range(size_s2):
-            m[0,j] = j 
+            m[0,j] = j
 
         for i in range(1,size_s1):
             for j in range(1,size_s2):
@@ -97,7 +101,7 @@ class Ayudante_Ortografico(object):
                 jmenos1 = m[i,j-1] + 1 #[i,j-1]
                 m[i,j] = min(imenos1,imenos1_jmenos1,jmenos1)
         return (int(m[size_s1 - 1, size_s2 - 1]))
-    
+
     "corregirTexto recibe como entrada un archivo co"
     def corregirTexto(self,finput):
         archivo = open(finput,"r")
@@ -107,8 +111,8 @@ class Ayudante_Ortografico(object):
         for linea in documento:
             #devuelve una lista con las palabras en el string, utilizando un separador #especificado como delimitador entre palabras
             linea = linea.strip('\n').split(" ")
-            
-            for palabra in linea:  
+
+            for palabra in linea:
                 valida = esPalabraValida(palabra)
                 if valida == True :
                     index = self.alfabeto.index(palabra[0])
@@ -121,34 +125,28 @@ class Ayudante_Ortografico(object):
 
         NoDicc = sorted(NoDicc)
         foutput = None
-        if foutput: 
+        if foutput:
             ArchivoSalida = open(foutput, "w")
         else:
             ArchivoSalida = open("foutput.txt" ,"w")
 
         for palabra in NoDicc:
-            print()
-            print("palabra", palabra)
-            print()
+
             m = self.sugerencia(palabra)
-            print()
-            print(m)
+
             salida = ",".join(m)
             #escribiendo archivo de salida
             ArchivoSalida.write(f"{palabra},{salida}"+"\n")
         ArchivoSalida.close()
-                                       
-        
-    
+
+    "sugerencia es un metodo auxiliar que tiene como tarea devolver las  cuatro palabras con menor distancia levensthein que seran las palabras que va a sugerir el ayudante ortografico"
     def sugerencia(self,elemento:str):
 
-        Arreglo = []           
+        Arreglo = []
         for diccionario in self.dicc:
             for palabra in diccionario.palabras.tabla:
                 if palabra != None:
                     Arreglo.append(palabra)
-        print(Arreglo)
-        print()
         sugerencia = []
 
         for i in range(len(Arreglo)):
@@ -163,25 +161,29 @@ class Ayudante_Ortografico(object):
         for i in range(4):
             a.append(Orden[i])
         return a
+    "imprimirDiccionario tiene como funcion mostrar las palabras del diccionario en orden lexicografico"
 
+    def imprimirDiccionario(self):
+        assert(True)
+        print("#----------------Diccionario---------------#")
+        for diccionario in self.dicc:
+            if diccionario.palabras:
+                r = (diccionario.mostrarPalabras())
+        print(r)
+    
 
-
-    def __str__(self):
-        return f"{self.dicc}"
 
 
 a = Ayudante_Ortografico()
-
+a.crearAyudante
 a.cargarDiccionario('prueba.txt')
-for diccionario in a.dicc:
-    if diccionario.palabras:
-        diccionario.mostrarPalabras()
 #a.levenshtein_distance("Amaranta","barbara")
-a.borrarPalabra("queso")
-a.borrarPalabra("yate")
+#a.borrarPalabra("queso")
+#a.borrarPalabra("yate")
 #a.sugerencia("auto")
-#a.corregirTexto("corregir.txt")
-for diccionario in a.dicc:
-    if diccionario.palabras:
-        diccionario.mostrarPalabras()
-
+inicio = perf_counter()
+a.corregirTexto("corregir.txt")
+#a.imprimirDiccionario()
+final = perf_counter()
+time = final - inicio
+print(f" tiempo de corregir texto {time}")
